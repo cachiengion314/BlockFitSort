6d7b3d2a894140ff4b75ce2053f517c3a4a550fd
HoangNam <hnam143@gmail.com>
2025-09-09 15:38:49 +0700

    update

--- Assets/Game/Scripts/Managers/LevelSystem/SpawnObjsSystem.cs
+++ Assets/Game/Scripts/Managers/LevelSystem/SpawnObjsSystem.cs
@@ -1,5 +1,3 @@
-using System.Collections.Generic;
-using TMPro;
 using Unity.Mathematics;
 using UnityEngine;
 
@@ -8,6 +6,7 @@ public partial class LevelSystem : MonoBehaviour
   [SerializeField] Transform spawnedParent;
   [SerializeField] Transform tubePref;
   [SerializeField] Transform blockPref;
+  [SerializeField] Transform cube2DPref;
 
   Transform SpawnTube(float3 localPos)
   {
@@ -19,6 +18,13 @@ public partial class LevelSystem : MonoBehaviour
   Transform SpawnBlock(float3 localPos)
   {
     var obj = Instantiate(blockPref, spawnedParent);
+    obj.localPosition = localPos;
+    return obj;
+  }
+
+  Transform SpawnCube2D(float3 localPos)
+  {
+    var obj = Instantiate(cube2DPref, spawnedParent);
     obj.localPosition = localPos;
     return obj;
   }
